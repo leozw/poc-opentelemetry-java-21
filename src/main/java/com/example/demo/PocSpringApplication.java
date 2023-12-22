c8d7ea08c0f9f71329ae0f4ab63b0e5e8652bf46
leozw <lzwdeveloper@gmail.com>
2023-12-22 13:46:55 -0300

    feat: app java with Otel instrumentation and exporter with Otel collector

---
+++ src/main/java/com/example/demo/PocSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringApplication.class, args);
+	}
+
+}
